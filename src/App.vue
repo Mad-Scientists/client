76916561ee2926b6c9b0b15ef782d137d99cbfe4
Cassandra Torske <cassandra.torske@gmail.com>
2017-08-07 12:14:34 -0600

    :tada: initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app id="app" standalone>
+    <!-- <v-toolbar fixed>
+      <v-toolbar-title>Bestie</v-toolbar-title>
+    </v-toolbar> -->
+    <main>
+      <v-container>
+        <router-view></router-view>
+        <!--v-router-->
+      </v-container>
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+body {
+  font-family: 'Gudea', sans-serif;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'Quicksand', sans-serif;
+}
+
+.bestie {
+  font-family: 'Pacifico', cursive;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs4>
+      <v-card dark class="primary">
+        <v-card-text>
+          <p>
+            chat
+          </p>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+
+
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex xs8>
+      <h2 class="text-md-center bestie">Bestie</h2>
+      <h3 class="text-md-center">Your new best friend.</h3>
+        <center>
+          <img src="https://avatars0.githubusercontent.com/u/30674113?v=4&s=200" />
+
+        </center>
+      <p class="text-md-center">Bestie will start a conversation with you via text message when you most need it. Bestie knows your interests and remembers past conversations. Bestie asks interesting and topical questions to keep the conversation going.</p>
+    </v-flex>
+    <v-flex xs4 class="text-md-center mt-3">
+      <i class="material-icons" style="font-size: 4em">grade</i>
+      <h5>key feature 1</h5>
+    </v-flex>
+    <v-flex xs4 class="text-md-center mt-3">
+      <i class="material-icons" style="font-size: 4em">grade</i>
+      <h5>key feature 2</h5>
+    </v-flex>
+    <v-flex xs4 class="text-md-center mt-3">
+      <i class="material-icons" style="font-size: 4em">grade</i>
+      <h5>key feature 3</h5>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
