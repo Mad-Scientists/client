2fbc2095d86553dcb44216a3d61684eac24fdbc0
Cassandra Torske <cassandra.torske@gmail.com>
2017-08-08 19:31:53 -0600

    :sparkles: add initial dialogue

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,5 +1,237 @@
 <template lang="html">
-  <h3>profile</h3>
+        <v-container>
+          <v-layout row>
+            <v-flex>
+              <v-card class="grey pa-2">
+                <v-card class="white white--text pa-2">
+                  <v-card-title primary-title>
+                    <div class="text-xs-center"> <h5>Bestie</h5></div>
+                  </v-card-title>
+                  <div class="pl-3 pr-3 pb-3">
+                    <hr>
+                  </div>
+                  <div class="scroll-it">
+
+                  <v-layout>
+                    <v-flex xs1 offset-xs2>
+                      <v-list-tile-avatar>
+                        <img src="http://fuuse.net/wp-content/uploads/2016/02/avatar-placeholder.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                    <v-flex xs6>
+                      <v-chip>Hello, I'm Bestie!</v-chip>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout>
+                    <v-flex xs1 offset-xs2>
+                      <v-list-tile-avatar>
+                        <img src="http://fuuse.net/wp-content/uploads/2016/02/avatar-placeholder.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                    <v-flex xs6>
+                      <v-chip class="chips">But you can call me something different, if you want. </v-chip>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout>
+                    <v-flex xs1 offset-xs2>
+                      <v-list-tile-avatar>
+                        <img src="http://fuuse.net/wp-content/uploads/2016/02/avatar-placeholder.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                    <v-flex xs6>
+                      <v-chip class="chips">What would you like to call me?</v-chip>
+                    </v-flex>
+                  </v-layout>
+                  <!-- *** USER TEXTS HERE FAM *** -->
+                  <v-layout>
+                    <v-flex xs6 offset-xs2 class="text-xs-left">
+                      <v-chip class="teal lighten-4 chips">Jim-Bob McGruff Elizabeth Montgomery</v-chip>
+                    </v-flex>
+                    <v-flex xs1>
+                      <v-list-tile-avatar>
+                      <img src="http://modexenergy.com/wp-content/themes/modex_wp/img/avatar.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                  </v-layout>
+                  <!-- *** USER TEXTS ABOVE FAM *** -->
+                  <v-layout>
+                    <v-flex xs1 offset-xs2>
+                      <v-list-tile-avatar>
+                        <img src="http://fuuse.net/wp-content/uploads/2016/02/avatar-placeholder.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                    <v-flex xs6>
+                      <v-chip class="chips">'Jim-Bob McGruff Elizabeth Montgomery'! I like that. Is this what you want to call me?</v-chip>
+                    </v-flex>
+                  </v-layout>
+                  <!-- *** USER TEXTS HERE FAM *** -->
+                  <v-layout>
+                    <v-flex xs6 offset-xs2 class="text-xs-left">
+                      <v-chip class="teal lighten-4 chips">Yes</v-chip>
+                    </v-flex>
+                    <v-flex xs1>
+                      <v-list-tile-avatar>
+                      <img src="http://modexenergy.com/wp-content/themes/modex_wp/img/avatar.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout>
+                    <v-flex xs1 offset-xs2>
+                      <v-list-tile-avatar>
+                        <img src="http://fuuse.net/wp-content/uploads/2016/02/avatar-placeholder.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                    <v-flex xs6>
+                      <v-chip class="chips">Great! What should I call you?</v-chip>
+                    </v-flex>
+                  </v-layout>
+                  <!-- *** USER TEXTS HERE FAM *** -->
+                  <v-layout>
+                    <v-flex xs6 offset-xs2 class="text-xs-left">
+                      <v-chip class="teal lighten-4 chips">CJ</v-chip>
+                    </v-flex>
+                    <v-flex xs1>
+                      <v-list-tile-avatar>
+                      <img src="http://modexenergy.com/wp-content/themes/modex_wp/img/avatar.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout>
+                    <v-flex xs1 offset-xs2>
+                      <v-list-tile-avatar>
+                        <img src="http://fuuse.net/wp-content/uploads/2016/02/avatar-placeholder.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                    <v-flex xs6>
+                      <v-chip class="chips">Hello, CJ! Did I spell that right?</v-chip>
+                    </v-flex>
+                  </v-layout>
+                  <!-- *** USER TEXTS HERE FAM *** -->
+                  <v-layout>
+                    <v-flex xs6 offset-xs2 class="text-xs-left">
+                      <v-chip class="teal lighten-4 chips">Yes</v-chip>
+                    </v-flex>
+                    <v-flex xs1>
+                      <v-list-tile-avatar>
+                      <img src="http://modexenergy.com/wp-content/themes/modex_wp/img/avatar.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout>
+                    <v-flex xs1 offset-xs2>
+                      <v-list-tile-avatar>
+                        <img src="http://fuuse.net/wp-content/uploads/2016/02/avatar-placeholder.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                    <v-flex xs6>
+                      <v-chip class="chips">Jim-Bob McGruff Elizabeth Montgomery and CJ. That's got a nice ring to it.</v-chip>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout>
+                    <v-flex xs1 offset-xs2>
+                      <v-list-tile-avatar>
+                        <img src="http://fuuse.net/wp-content/uploads/2016/02/avatar-placeholder.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                    <v-flex xs6>
+                      <v-chip class="chips">I will send you messages throughout the day.</v-chip>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout>
+                    <v-flex xs1 offset-xs2>
+                      <v-list-tile-avatar>
+                        <img src="http://fuuse.net/wp-content/uploads/2016/02/avatar-placeholder.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                    <v-flex xs6>
+                      <v-chip class="chips">What is the first time of day I should contact you?</v-chip>
+                    </v-flex>
+                  </v-layout>
+                  <!-- *** USER TEXTS HERE FAM *** -->
+                  <v-layout>
+                    <v-flex xs6 offset-xs2 class="text-xs-left">
+                      <v-chip class="teal lighten-4 chips">7AM</v-chip>
+                    </v-flex>
+                    <v-flex xs1>
+                      <v-list-tile-avatar>
+                      <img src="http://modexenergy.com/wp-content/themes/modex_wp/img/avatar.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout>
+                    <v-flex xs1 offset-xs2>
+                      <v-list-tile-avatar>
+                        <img src="http://fuuse.net/wp-content/uploads/2016/02/avatar-placeholder.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                    <v-flex xs6>
+                      <v-chip class="chips">Nice and early. I like it.</v-chip>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout>
+                    <v-flex xs1 offset-xs2>
+                      <v-list-tile-avatar>
+                        <img src="http://fuuse.net/wp-content/uploads/2016/02/avatar-placeholder.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                    <v-flex xs6>
+                      <v-chip class="chips">What other time of day should I contact you?</v-chip>
+                    </v-flex>
+                  </v-layout>
+                  <!-- *** USER TEXTS HERE FAM *** -->
+                  <v-layout>
+                    <v-flex xs6 offset-xs2 class="text-xs-left">
+                      <v-chip class="teal lighten-4 chips">10AM</v-chip>
+                    </v-flex>
+                    <v-flex xs1>
+                      <v-list-tile-avatar>
+                      <img src="http://modexenergy.com/wp-content/themes/modex_wp/img/avatar.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout>
+                    <v-flex xs1 offset-xs2>
+                      <v-list-tile-avatar>
+                        <img src="http://fuuse.net/wp-content/uploads/2016/02/avatar-placeholder.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                    <v-flex xs6>
+                      <v-chip class="chips">OK! I'll send you a message everyday at 7AM and 10AM.</v-chip>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout>
+                    <v-flex xs1 offset-xs2>
+                      <v-list-tile-avatar>
+                        <img src="http://fuuse.net/wp-content/uploads/2016/02/avatar-placeholder.png"/>
+                      </v-list-tile-avatar>
+                    </v-flex>
+                    <v-flex xs6>
+                      <v-chip class="chips">There are a few more ways to customize me. Click here to update your preferences.</v-chip>
+                    </v-flex>
+                  </v-layout>
+                  </div>
+                  <div class="pa-3">
+                    <hr>
+                  </div>
+                <v-layout>
+                <v-flex xs6 offset-xs2>
+                  <v-text-field
+                 label="Text"
+               ></v-text-field>
+                </v-flex>
+                  <v-flex xs4>
+                    <v-card-actions>
+                      <v-btn>Send</v-btn>
+                    </v-card-actions>
+                  </v-flex>
+                </v-layout>
+              </v-card>
+            </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </main>
+    </v-app>
 </template>
 
 <script>
@@ -8,4 +240,18 @@ export default {
 </script>
 
 <style lang="css">
+
+span, .chips {
+    display: block;
+    word-wrap:break-word;
+    white-space: pre-wrap;
+    height: auto;
+    padding: 1em;
+}
+
+.scroll-it {
+  max-height: 40vh;
+  overflow: scroll;
+}
+
 </style>
