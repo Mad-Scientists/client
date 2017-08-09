decffd89666459bbcc877e14f1a7fe398537f52e
CJ R <cj@null.computer>
2017-08-09 10:58:14 -0600

    :bug: https

--- src/components/BotMessage.vue
+++ src/components/BotMessage.vue
@@ -2,7 +2,7 @@
   <v-layout>
     <v-flex xs1 offset-xs2>
       <v-list-tile-avatar>
-        <img :src="options.bot_name == 'Bestie' ? 'https://avatars0.githubusercontent.com/u/30674113?v=4&s=200' : options.bot_name ? 'https://api.adorable.io/avatars/285/' + options.bot_name + '.png' : 'http://i.imgur.com/XV5dMmb.png'" />
+        <img :src="options.bot_name == 'Bestie' ? 'https://avatars0.githubusercontent.com/u/30674113?v=4&s=200' : options.bot_name ? 'https://api.adorable.io/avatars/285/' + options.bot_name + '.png' : 'https://i.imgur.com/XV5dMmb.png'" />
       </v-list-tile-avatar>
     </v-flex>
     <v-flex xs6>
